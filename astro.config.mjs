2c27ed51de6bc1cfbbd513751be66d1a9b1b3114
DavidCorreaV <david.correav@gmail.com>
2022-09-05 08:39:21 -0500

    basic scaffolding for the blog project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwind from 'astrojs/tailwind';
 
 import sitemap from '@astrojs/sitemap';
+import storyblok from '@astrojs/storyblok';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), tailwind(), storyblok({
+		accessToken: "To2THN2L0JymhIMDLO6fbgtt",
+		components: {
+		  page: "storyblok/Page",
+	  feature: "storyblok/Feature",
+	  grid: "storyblok/Grid",
+	  teaser: "storyblok/Teaser",
+		}
+	  }),],
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+      './public/**/*.html',
+      './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+    ],
+    theme: {
+      container: {
+        center: true,
+        padding: '1.5rem',
+      },
+    },
+  }
