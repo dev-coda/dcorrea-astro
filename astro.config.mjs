0ac9db79d62564ce96a730922350b86943123818
DavidCorreaV <david.correav@gmail.com>
2022-09-11 20:50:38 -0500

    Added vercel integration for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,17 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import image from "@astrojs/image";
-
 import compress from "astro-compress";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dcorrea.co",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    preact({
-      compat: true,
-    }),
-    image(),
-    compress(),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), preact({
+    compat: true
+  }), image(), compress()],
   output: "static",
   root: "./",
+  adapter: vercel()
 });
